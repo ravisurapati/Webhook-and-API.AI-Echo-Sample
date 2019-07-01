eec0839146193d9be2abe9fc961f20eb1eecde9c
Ravi Surapati <ravisurapati@gmail.com>
2019-07-01 17:23:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,39 @@ const bodyParser = require("body-parser");
 
 const restService = express();
 
-restService.use(bodyParser.urlencoded({
+restService.use(
+  bodyParser.urlencoded({
     extended: true
-  }));
+  })
+);
 
 restService.use(bodyParser.json());
+
 restService.post("/echo", function(req, res) {
-  var speech = req.body.result && req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText;
+  var temp = {
+    google: {
+      expectUserResponse: true,
+      richResponse: {
+        items: [
+          {
+            simpleResponse: {
+              textToSpeech: "this is a simple response"
+            }
+          }
+        ]
+      }
+    }
+  };
+  var speech =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.echoText
+      ? req.body.queryResult.parameters.echoText
+      : "Seems like some problem. Speak again.";
   return res.json({
+    payload: temp,
+    data: temp,
+    fulfillmentText: "Sample response",
     speech: speech,
     displayText: speech,
     source: "webhook-echo-sample"
